f06786ad0cff872af95c383c50be746352b99ca9
ikairat <ik.kairat@gmail.com>
2021-09-28 07:32:20 +0600

    some editing

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -8,6 +11,10 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
+
+type APIResponse struct {
+	Response interface{}
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -22,7 +29,41 @@ func main() {
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+		body, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(body))
+
+		apiResp := APIResponse{}
+		err = json.Unmarshal(body, &apiResp)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(apiResp.Response)
+
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
+			"response": apiResp,
+		})
+	})
+
+	router.GET("/callback", func(c *gin.Context) {
+		body, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(body))
+
+		apiResp := APIResponse{}
+		err = json.Unmarshal(body, &apiResp)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(apiResp.Response)
+
+		c.HTML(http.StatusOK, "index.tmpl.html", gin.H{
+			"response": apiResp,
+		})
 	})
 
 	router.Run(":" + port)
